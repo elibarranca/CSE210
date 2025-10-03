abd6e8d3d1bd32b61106080e45f3743e9881f5c5
elibarranca <elisabarranca.99@gmail.com>
2025-10-02 23:51:21 -0600

    homework

---
+++ prove/Develop03/Job.cs
@@ -0,0 +1,16 @@
+using System;
+public class Job
+{
+    public string _jobTitle;
+
+    public string _company;
+
+    public int _startYear;
+
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"Job Title: {_jobTitle}, Company: {_company}, Start Year: {_startYear}, End Year: {_endYear}");
+    }
+}

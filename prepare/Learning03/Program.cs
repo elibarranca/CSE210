59ba4f6feb434c715a221c675be3ca9391e956ab
elibarranca <elisabarranca.99@gmail.com>
2025-10-28 21:28:35 -0600

    class work

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+        Schedule student = new Student("Gary", "1234");
+        Schedule schedule = new Schedule(student);
+
+
+        while (keepRunning)
+        {
+            Console.WriteLine("1. Display");
+            Console.WriteLine("2. Add Course");
+            Console.WriteLine("Choose option:");
+
+            string response = Console.ReadLine();
+
+            if (response == "1")
+            {
+                schedule.Display();
+            }
+            else if (response == "2")
+            {
+                Console.WriteLine("Course number?");
+                string courseNumber = Console.ReadLine();
+                Course course = new Course(courseNumber, 0, 0, "M/W");
+                schedule.AddCourse(course);
+            }
+
+        }
     }
 }
---
+++ prepare/Learning03/Student.cs
@@ -0,0 +1,17 @@
+public class Student
+{
+    private string _name;
+
+    private string _id;
+
+    public Student(string name, string id)
+    {
+        _name = name;
+        _id = id;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Student: {_name} with id: {_id}");
+    }
+}
